99d0b511ebf2bb1fbf157836fabbd1ae118956a8
Vlad <vladpredator@gmail.com>
2025-04-23 11:47:01 +0300

    modified tests in file test_logger.py

--- lesson_10/test_logger.py
+++ lesson_10/test_logger.py
@@ -4,59 +4,49 @@ from lesson_10.homework_10 import log_event, get_last_username, get_last_status,
 
 class TestLogger:
 
-    def test_log_username(self):
+    def test_log_status_success(self):
         """
-        Creating a new log message in file and checking if username in a message equals to expected username
-        """
-        username: str = "Vlad"
-        status: str = "success"
-        log_event(username=username, status=status)
-
-        assert username == get_last_username(), f"Actual username:{username} does not equal to expected username:{get_last_username()}"
-
-
-    def test_log_status(self):
-        """
-        Creating a new log message in file and checking if status in a message equals to expected status
-        """
-        username: str = "Dmytro"
-        status: str = "expired"
-        log_event(username=username, status=status)
-
-        assert status == get_last_status(), f"Actual status:{status} does not equal to expected status:{get_last_status()}"
-
-
-    def test_log_level_info(self):
-        """
-        Creating a new log message in file with Success status and checking if log level used in a message equals to expected log level
+        Creating a new log message in file with Success status,
+        checking if username, status in a message equals to expected username and status,
+        also checking if log level used in a message equals to Info log level
         """
         username: str = "Cody"
         status: str = "success"
         level: str = "INFO"
         log_event(username=username, status=status)
 
+        assert status == get_last_status(), f"Actual status:{status} does not equal to expected status:{get_last_status()}"
+        assert username == get_last_username(), f"Actual username:{username} does not equal to expected username:{get_last_username()}"
         assert level == get_last_log_level(), f"Actual log level:{level} does not equal to expected log level:{get_last_log_level()}"
 
 
-    def test_log_level_warning(self):
+    def test_log_status_expired(self):
         """
-        Creating a new log message in file with Warning status and checking if log level used in a message equals to expected log level
+        Creating a new log message in file with Expired status,
+        checking if username, status in a message equals to expected username and status,
+        also checking if log level used in a message equals to Warning log level
         """
         username: str = "David"
         status: str = "expired"
         level: str = "WARNING"
         log_event(username=username, status=status)
 
+        assert status == get_last_status(), f"Actual status:{status} does not equal to expected status:{get_last_status()}"
+        assert username == get_last_username(), f"Actual username:{username} does not equal to expected username:{get_last_username()}"
         assert level == get_last_log_level(), f"Actual log level:{level} does not equal to expected log level:{get_last_log_level()}"
 
 
-    def test_log_level_error(self):
+    def test_log_status_failed(self):
         """
-        Creating a new log message in file with Error status and checking if log level used in a message equals to expected log level
+        Creating a new log message in file with Failed status,
+        checking if username, status in a message equals to expected username and status,
+        also checking if log level used in a message equals to Error log level
         """
         username: str = "Max"
         status: str = "failed"
         level: str = "ERROR"
         log_event(username=username, status=status)
 
+        assert status == get_last_status(), f"Actual status:{status} does not equal to expected status:{get_last_status()}"
+        assert username == get_last_username(), f"Actual username:{username} does not equal to expected username:{get_last_username()}"
         assert level == get_last_log_level(), f"Actual log level:{level} does not equal to expected log level:{get_last_log_level()}"
