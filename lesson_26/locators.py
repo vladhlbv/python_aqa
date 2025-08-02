4d97ac0615f05a3dad9180bf22aff8c04d33ea6b
Vlad <vladpredator@gmail.com>
2025-08-02 17:16:33 +0300

    added test_selenium_frames.py, locators.py, conftest.py files

---
+++ lesson_26/locators.py
@@ -0,0 +1,4 @@
+frame1_text = """//input[@id='input1']"""
+frame1_check_button = """//button[@onclick="verifyInput('input1')"]"""
+frame2_text = """//input[@id='input2']"""
+frame2_check_button = """//button[@onclick="verifyInput('input2')"]"""
